727c68f67fb54240ef628fb6db54064ae780c79c
haonguyennhat <162509494+haonguyen270225@users.noreply.github.com>
2025-02-20 01:15:20 +0700

    Chỉnh hợp chạp k cua n không lặp

--- thuat_toan_quay_lui_01.cpp
+++ thuat_toan_quay_lui_01.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 using namespace std;
 int n,k , x[101], use[101];
+
 void InKQ() {
 	for (int i = 1; i <= k; i++) {
 		cout << x[i];
 	}
 	cout << endl;
 }
+
 void Try( int i) {
 	for (int j = 1; j <= n; j++) {
 		if (use[j] == 0) {
@@ -21,15 +23,14 @@ void Try( int i) {
 				
 				Try(i + 1);
 			}
+			// back_track
 			use[j] = 0;
 		}
 	}
 }
-
-
 int main() {
 	memset(use, 0, sizeof(use));
-	cout << "Nhap n va k tu ban phim :";
+	cout << "Nhap k va n tu ban phim :";
 	cin >> k >> n;
 	Try(1);
 	return 0;
