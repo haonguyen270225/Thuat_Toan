487b77c20450d60e563a4f5a90322056513f4876
haonguyennhat <162509494+haonguyen270225@users.noreply.github.com>
2025-02-20 01:11:01 +0700

    thuat_toan_quay_lui_01

---
+++ demo.cpp
@@ -0,0 +1,36 @@
+﻿
+//Chinh hop chap k cua n khong lap;
+#include <iostream>
+using namespace std;
+int n,k , x[101], use[101];
+void InKQ() {
+	for (int i = 1; i <= k; i++) {
+		cout << x[i];
+	}
+	cout << endl;
+}
+void Try( int i) {
+	for (int j = 1; j <= n; j++) {
+		if (use[j] == 0) {
+			x[i] = j;
+			use[j] = 1;
+			if (i == k) {
+				InKQ();
+			}
+			else {
+				
+				Try(i + 1);
+			}
+			use[j] = 0;
+		}
+	}
+}
+
+
+int main() {
+	memset(use, 0, sizeof(use));
+	cout << "Nhap n va k tu ban phim :";
+	cin >> k >> n;
+	Try(1);
+	return 0;
+}
