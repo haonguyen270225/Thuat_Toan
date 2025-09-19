d5b180969d3c64c2d681c3a77f38d577b365f738
haonguyennhat <haonguyennhat270225@gmail.com>
2025-09-20 00:32:55 +0700

    Learn Algorithms

---
+++ DSA(Data Structures and Algorithms)/SortingAlgorithms/BubbleSort_1.cpp
@@ -0,0 +1,34 @@
+﻿// Bubble Sort : Sắp xếp noi bọt . Sử dụng vòng lặp.
+#include <iostream>
+#include <vector>
+using namespace std;
+void BubbleSort(vector <int>& arr) {
+	bool swapped;
+	int n = arr.size();
+	for (int i = 0; i < n - 1; i++) {
+		swapped = false;
+		for (int j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+				swapped = true;
+			}
+		}
+		// Nếu có chưa có swap thì ngưng;
+		if (swapped == false) {
+			break;
+		}
+	}
+}
+void PrintArray(vector <int> arr) {
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+}
+int main() {
+	vector <int> arr = { 10 , 4 , 5 , 11 , 90 , 3 , 1 };
+	BubbleSort(arr);
+	cout << "Mang sau khi sap xep :\n";
+	PrintArray(arr);
+	return 0;
+}
---
+++ DSA(Data Structures and Algorithms)/SortingAlgorithms/BubbleSort_2.cpp
@@ -0,0 +1,36 @@
+﻿// Bubble Sort : Sắp xếp nổi bọt. Sử dụng đệ quy .
+// Độ phức tạp thời gian : O(n^2)
+// Không gia phụ trợ : O(n)
+#include <iostream>
+#include <vector>
+using namespace std;
+void PrintArray(vector <int> arr) {
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+}
+void BubbleSort(vector <int> &arr , int n) {
+	// Trường hợp cơ sở (Base Case);
+	if (n == 1) return;
+	int count = 0;
+	for (int i = 0; i < n - 1; i++) {
+		if (arr[i] > arr[i + 1]) {
+			swap(arr[i], arr[i + 1]);
+			count++;
+		}
+	}
+	// Kiểm tra xem có xảy ra hóng đổi không;
+	if (count == 0) return;
+	BubbleSort(arr, n - 1);
+}
+
+
+int main() {
+	vector <int> arr = { 10 , 5 , 42 , 21 , 90 , 2 , 1 };
+	int n = arr.size();
+	BubbleSort(arr, n);
+	cout << "Mang sau khi sap xep la :\n";
+	PrintArray(arr);
+	return 0;
+}
